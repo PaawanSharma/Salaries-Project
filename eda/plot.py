7ffd6d29c39bf03eccb796d0c455995ee7239136
Paawan Sharma <paawansharma@protonmail.com>
2021-02-11 17:09:47 +0000

    Rearrange plotting code

--- eda/plot.py
+++ eda/plot.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-def plot_target(target, dataframe, hist_bins=20):
+def plot_target(target, dataframe, hist_bins=20, target_label=None):
     """Plot a seaborn boxplot and histogram for the target variable.
 
     Keyword Arguments:
@@ -12,18 +12,24 @@ def plot_target(target, dataframe, hist_bins=20):
     target -- the target variable
     dataframe -- the pandas DataFrame containing the data
     hist_bins -- the number of bins to use in the histogram plot
+    target_label -- a label for the target axis
     """
-    target_fig = plt.figure(figsize=(16, 10))
+    target_fig = plt.figure(figsize=(16, 16))
 
-    target_box = target_fig.add_subplot(2, 1, 1)
-    sns.boxplot(dataframe[target],
-                ax=target_box)
+    box_ax = target_fig.add_subplot(2, 1, 1)
+    target_box = sns.boxplot(dataframe[target],
+                flierprops={'markerfacecolor': 'white'},
+                ax=box_ax)
 
-    target_hist = target_fig.add_subplot(2, 1, 2)
-    sns.histplot(dataframe[target],
+    hist_ax = target_fig.add_subplot(2, 1, 2)
+    target_hist = sns.histplot(dataframe[target],
                  bins=hist_bins,
                  kde=True,
-                 ax=target_hist)
+                 ax=hist_ax)
+    
+    if target_label:
+        for plotted in [target_box, target_hist]:
+            plotted.set(xlabel=target_label)
 
 
 def plot_categorical(feature, target, dataframe):
@@ -49,11 +55,26 @@ def plot_categorical(feature, target, dataframe):
                           y=target,
                           data=dataframe,
                           order=ascending,
+                          flierprops={'markerfacecolor': 'white'},
                           ax=box_ax)
 
-    if feature == 'companyId':
-        cat_count.set_xticklabels([])
-        cat_box.set_xticklabels([])
+    for plotted in [cat_count, cat_box]:
+        categoricals_axes(plotted, feature, "Salary / 1000 USD")
+
+
+def categoricals_axes(plotted, feature, target_label=None):
+    """Stylise plot axes for improved visual clarity.
+
+    Keyword Arguments:
+    -----------------
+    plotted -- the plot object
+    feature -- the feature that was plotted
+    target_label -- a label for the target axis
+    """
+    if feature == "companyId":
+        plotted.set_xticklabels([])
     else:
-        cat_count.set_xticklabels(cat_count.get_xticklabels(), rotation=36)
-        cat_box.set_xticklabels(cat_count.get_xticklabels(), rotation=36)
+        plotted.set_xticklabels(plotted.get_xticklabels(), rotation=36)
+
+    if target_label:
+        plotted.set(ylabel=target_label)
