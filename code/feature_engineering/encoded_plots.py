2cd0c01095a854f13d82f17d43e41c20b24e74b7
Paawan Sharma <paawansharma@protonmail.com>
2021-02-15 02:02:15 +0000

    Fix style for encoded_plots.py

--- code/feature_engineering/encoded_plots.py
+++ code/feature_engineering/encoded_plots.py
@@ -1,21 +1,44 @@
+"""Functions for generating plots from encodings."""
+
 from matplotlib.pyplot import figure, show
 from seaborn import set_context, heatmap
 from numpy import zeros_like, triu_indices_from
 
+
 def get_corr_matrix(features, dataframe, exclude, encoder_func, reorder=False):
+    """Return correlation matrix for a particular encoding.
+
+    Keyword Arguments:
+    -----------------
+    features --
+    dataframe --
+    exclude --
+    encoder_func --
+    reorder --
+    """
     encoded = encoder_func(features, dataframe, exclude)
     corr_matrix = encoded.corr()
-    
+
     if reorder:
         reordered_index = corr_matrix.columns[[11, 5, 13, 20, 22, 1, 6,
-                   8, 26, 27, 7, 24, 25, 3,
-                   23, 4, 0, 2, 9, 18, 17,
-                   16, 12, 21, 15, 19, 10, 14]]
+                                               8, 26, 27, 7, 24, 25, 3,
+                                               23, 4, 0, 2, 9, 18, 17,
+                                               16, 12, 21, 15, 19, 10, 14]]
         corr_matrix = corr_matrix.reindex(index=reordered_index,
                                           columns=reordered_index)
     return corr_matrix
 
+
 def heat_map(matrix, cmap, size, fontsize):
+    """Produce heatmap.
+
+    Keyword Arguments:
+    -----------------
+    matrix --
+    cmap --
+    size --
+    fontsize --
+    """
     mask = zeros_like(matrix)
     mask[triu_indices_from(mask)] = 1
     set_context('poster', font_scale=fontsize)
@@ -32,4 +55,3 @@ def heat_map(matrix, cmap, size, fontsize):
             cbar=False,
             figure=fig)
     show()
-    
