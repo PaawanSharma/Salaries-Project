340f16d21341c12f14ce0798fcb9e62d1ecd7750
Paawan Sharma <paawansharma@protonmail.com>
2021-02-15 19:04:49 +0000

    Add label encoding and heatmap

--- code/feature_engineering/encoding.py
+++ code/feature_engineering/encoding.py
@@ -60,3 +60,36 @@ have the same {} {}.".format(feature,
     dataframe[feature] = dataframe[feature].replace(order, range(len(order)))
 
     return ordinal_encode(features[1:], dataframe, metric, target)
+
+
+def label_encode(features, dataframe, metric, target):
+    """Apply label encoding.
+
+    Keyword Arguments:
+    -----------------
+    features -- a list of categorical features for encoding
+    target -- the target variable
+    metric -- the function for evaluating the target in relation to each group
+    dataframe -- the pandas DataFrame containing the data
+    """
+    dataframe = dataframe.copy()
+
+    if len(features) == 0:
+        return dataframe
+
+    feature = features[0]
+
+    metric_values = dataframe.groupby(feature)[target].\
+        apply(metric)
+
+    if not metric_values.is_unique:
+        raise NotUniqueError("Ordinal encoding cannot be executed with \
+these parameters as two or more groups of {} \
+have the same {} {}.".format(feature,
+                             target,
+                             metric.__name__))
+
+    mapping = metric_values.to_dict()
+    dataframe[feature].replace(mapping, inplace=True)
+
+    return label_encode(features[1:], dataframe, metric, target)
