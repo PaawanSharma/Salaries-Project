10903b5fa0b30101c919fe9102c6cb38276cc9de
Paawan Sharma <paawansharma@protonmail.com>
2021-02-20 01:52:38 +0000

    Add logging functionality for model tests

--- code/preprocessing.py
+++ code/preprocessing.py
@@ -8,13 +8,16 @@ def X_y_split(dataframe, target=None):
         X = dataframe.drop(target, axis=1)
     return X, y
 
+
 def encode(encoder, train, test):
     encoder.fit(train)
     new_train = encoder.transform(train)
     new_test = encoder.transform(test)
     return new_train, new_test
 
-def encode_and_split(encoder, train, test, target=None):
-    encoded_train, encoded_test = encode(encoder, train, test)
-    X_train, y_train = X_y_split(encoded_train, target)
-    return X_train, y_train, encoded_test
+
+def encode_and_split(train, test, target=None, encoder=None):
+    if encoder:
+        train, test = encode(encoder, train, test)
+    X_train, y_train = X_y_split(train, target)
+    return X_train, y_train, test
